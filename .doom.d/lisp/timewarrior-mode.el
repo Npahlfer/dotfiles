6c38fbf86fca32074f145cf80a583f4df9f60228
npahlfer <niclas@pahlfer.com>
2019-06-03 00:14:18 +0200

    [timewarrior-mode] Cache active state and active tag.

--- .doom.d/lisp/timewarrior-mode.el
+++ .doom.d/lisp/timewarrior-mode.el
@@ -6,9 +6,18 @@
 ;;; Code:
 
 
-; (defun get-current-tag-duration ()
-;   "Get timewarrior tag duratiuon."
-;   (shell-command "timew get dom.active.duration"))
+;; (defun get-current-tag-duration ()
+;;   "Get timewarrior tag duratiuon."
+;;   (substring
+;;    (car
+;;     (split-string
+;;      (shell-command-to-string "timew get dom.active.duration")
+;;      "M")
+;;     )
+;;    2))
+
+(defvar *is-active* nil "Cached active state.")
+(defvar *active-tag* nil "Cached active tag.")
 
 (defun is-active ()
   "Get timewarrior active state."
@@ -16,44 +25,45 @@
 
 (defun stop-tracker ()
   "Get timewarrior tag duratiuon."
-  (shell-command "timew stop"))
+  (shell-command "timew stop :quiet >> /dev/null"))
 
 (defun get-active-tag ()
   "Gets active timewarrior tag."
-  (string-trim-right (shell-command-to-string "timew get dom.active.tag.1")))
+  (if *active-tag*
+      *active-tag*
+    (string-trim-right (shell-command-to-string "timew get dom.active.tag.1"))))
 
 (defun set-active-tag (tag)
   "Set active timewarrior tag."
-  (progn (shell-command (format "timew start %s :quiet" tag)))
-    tag)
+  (progn
+    (shell-command (format "timew start %s :quiet >> /dev/null" tag))
+    (setq *active-tag* tag)
+    tag))
 
 (defun get-current-workspace ()
   "Get current workspace name."
   (safe-persp-name (+workspace-current)))
 
 (defun correct-tag (active-tag)
-  "Set active tag to the workspace name if it's not the same."
-  "Also check if the tracker is active otherwise activate it"
-  (let ((workspace-tag (get-current-workspace)))
-    (cond ((not (equal active-tag workspace-tag))
-           (set-active-tag workspace-tag))
-          ((not (is-active)) (set-active-tag active-tag)))
-    workspace-tag))
+  "Set active tag to the workspace name and set state to active."
+  (if (and *is-active* *active-tag*)
+      active-tag
+      (set-active-tag (get-current-workspace))))
 
 (defun get-mode-text ()
   "Get minor mode text"
   (concat " TW: " (correct-tag (get-active-tag))))
 
 (defun stop-timewarrior ()
-     (stop-tracker))
+  (progn
+    (setq *is-active* nil)
+    (stop-tracker)))
 
 (defun start-timewarrior ()
   (if (bound-and-true-p timewarrior-mode)
-        (correct-tag (get-active-tag))))
-
-(let ((workspace-tag (get-current-workspace)))
-  (if (not (equal "" workspace-tag))
-      (set-active-tag workspace-tag)))
+      (progn
+        (setq *is-active* t)
+        (correct-tag (get-active-tag)))))
 
 ;; Timewarrior Minor Mode
 (define-minor-mode timewarrior-mode
@@ -68,6 +78,8 @@
    :global 1
 )
 
+;; Make this async to prevent slow saves.
+;; And look for another hook
 (add-hook 'after-save-hook #'start-timewarrior)
 
 
